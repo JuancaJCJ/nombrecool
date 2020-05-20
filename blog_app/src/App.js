4e737ca045035dd650ce9b9c78bbb97e690b499c
LuciaGuerrero <freyaguza@hotmail.com>
2020-05-20 17:31:34 -0500

    actualizacion

--- blog_app/src/App.js
+++ blog_app/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import Blog from './components/Blog';
 
 function App() {
